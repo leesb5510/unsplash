a00e656faa6c72b9881e181533da93222e50d0be
Seung Bin Lee <leesb5510@gmail.com>
2024-10-14 14:57:25 +0900

    feat. Tanstack Query, Axios setting

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+export const customAxios = axios.create({
+  baseURL: import.meta.env.VITE_APP_API_URL,
+  headers: {
+    Authorization: `Client-ID ${import.meta.env.VITE_APP_CLIENT_ID}`,
+  },
+});
